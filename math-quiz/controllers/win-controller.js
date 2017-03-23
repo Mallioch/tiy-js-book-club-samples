722ec3eda5cd9ed7a79bec888a59935f627c9720
Eric Sowell <eric.sowell@gmail.com>
2017-03-22 22:20:32 -0500

    math quiz

---
+++ math-quiz/controllers/win-controller.js
@@ -0,0 +1,13 @@
+if (window.Quiz === undefined) window.Quiz = {};
+
+(function() {
+
+  var WinController = Controller.create({
+    render: function() {
+      this.el.html('<div class="win">win</a>');
+    }
+  });
+
+  Quiz.WinController = WinController;
+
+})();
